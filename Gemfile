62b717c1834b62bf1a854d673d13f467acc44026
bananaappletw <bananaappletw@gmail.com>
2018-03-22 12:48:47 +0800

    Add production build

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ group :jekyll_plugins do
   gem "font-awesome-sass"
   gem 'bulma-sass'
 end
+gem 'uglifier'
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-rm -rf security-onigiri.github.io/
-bundle exec jekyll build --destination=security-onigiri.github.io
-cd security-onigiri.github.io/
-git init
+rm -rf _site
+git clone git@github.com:security-onigiri/security-onigiri.github.io.git _site
+JEKYLL_ENV=production bundle exec jekyll build
+cd _site
 git add .
-git commit -m "Update"
-git remote add origin git@github.com:security-onigiri/security-onigiri.github.io.git
-git push -u origin master -f
+git commit --allow-empty -m "Update"
+git push
